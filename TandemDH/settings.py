b3e902ad9780b3d5f36a2c023f9f0303c52552c1
sreal19 <sbrnlb@gmail.com>
2015-04-13 06:26:55 -0400

    Fix bug in settings.py
    Bad construction of database locations

--- TandemDH/settings.py
+++ TandemDH/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TANDEM_DIR = os.getenv("HOME") + "/data/tandem.sqlite3"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -59,10 +60,10 @@ WSGI_APPLICATION = 'TandemDH.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, TANDEM_DIR),
+        'NAME': TANDEM_DIR,
     }
 }
-
+print DATABASES
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
