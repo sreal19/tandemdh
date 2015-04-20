587fb32234425f4b66b1aa2e3d79c7c3567d5f06
sreal19 <sbrnlb@gmail.com>
2015-04-20 18:15:54 -0400

    fix import of fields.py

--- tandem/forms.py
+++ tandem/forms.py
@@ -1,7 +1,7 @@
 __author__ = 'sbr'
 
 from django import forms
-from multiupload.fields import MultiFileField
+from fields import MultiFileField
 
 class MyUploadForm(forms.Form):
     attachments = MultiFileField(max_num=99, min_num=1, max_file_size=1024*1024*5,
